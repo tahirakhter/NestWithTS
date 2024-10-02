43a50e7d3cfffe2f79c22d6d5b2061229bd15643
tahirakhter <akhter_tahir@hotmail.com>
2024-10-02 12:05:47 +0500

    coderabbit integration for testing

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,19 +7,55 @@ import {
 } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { UserService } from "./user.service";
+import { PrismaService } from "../common/prisma/prisma.service";
+
 
 @UseGuards(AuthGuard("jwt"))
 @Controller("user")
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private prisma: PrismaService) {}
 
   @Get("getAllStudents")
   getAllStudentsByUser() {
+    try {
     return this.userService.getAllStudentsByUser();
+  } catch (e) {
+    throw new Error(e);
+  }
   }
 
   @Get(":id")
   getById(@Param("id", ParseIntPipe) id: number) {
+    try{
     return this.userService.getById(id);
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+
+  @Get("getAllStudents")
+  getAllStudentsByUser2() {
+    try {
+      const student = await this.prisma.user.findMany({
+        select: {
+          name: true,
+          email: true,
+          student: {
+            select: {
+              name: true,
+              studentId: true,
+              program: true,
+              phone: true,
+            },
+          },
+        },
+        orderBy: {
+          name: "desc",
+        },
+      });
+      return student;
+    } catch (e) {
+      throw new Error(e.message);
+    }
   }
 }
